85b3e77809f8c41a3c0663363409cc5f385ea2aa
Md Samiul Huq <samiulg3@gmail.com>
2014-07-03 18:45:28 -0700

    rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Brainstorm::Application.config.secret_token = 'e5e55659f2205c2a99228af32b941234bb52f15d45087b14e8339993a0de216316f92f1e728b68cec39c9bd206459aadf6ac8a27fad6096f1244da22fbf0a71a'
