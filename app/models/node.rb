1ce04209a0b888fffaa1bb29a1d03d1fe445397c
Md Samiul Huq <samiulg3@gmail.com>
2014-07-27 11:25:12 -0700

    validations

--- app/models/node.rb
+++ app/models/node.rb
@@ -1,4 +1,6 @@
 class Node < ActiveRecord::Base
  validates :source_body, :length: { maximum: 275, too_long: "You are only allowed a maximum of 275 characters" }, presence: true
  validates :target_body, :length: { maximum: 275, too_long: "You are only allowed a maximum of 275 characters" }, presence: true
+ validates :source, :target, :length: { maximum: 10, too_long: "You are only allowed a maximum of 10 characters" }
+ belongs_to :mindmap
 end
