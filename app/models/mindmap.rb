3fae8426d0098e8297076ef05809a1a013cf3c30
Md Samiul Huq <samiulg3@gmail.com>
2014-07-27 12:40:53 -0700

    minor changes

--- app/models/mindmap.rb
+++ app/models/mindmap.rb
@@ -1,7 +1,7 @@
 class Mindmap < ActiveRecord::Base
-  before_create: generate_token
-  validates: :unique_token, :uniqueness => true
-  has_many: :nodes
+  before_create :generate_token
+  validates :unique_token, :uniqueness => true
+  has_many :nodes
 
   def generate_token
     self.unique_token = SecureRandom.urlsafe_base64(6)
