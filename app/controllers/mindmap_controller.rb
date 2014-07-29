2bbf592a0bd0207819a7951b47e5da21e6bb0152
Md Samiul Huq <samiulg3@gmail.com>
2014-07-28 22:38:34 -0700

    show mindmap

--- app/controllers/mindmap_controller.rb
+++ app/controllers/mindmap_controller.rb
@@ -1,9 +1,14 @@
 class MindmapController < ApplicationController
+  def new
+    @mindmap = Mindmap.new
+    @node = Node.new
+  end
   def create
   @mindmap = Mindmap.new(params[:mindmap])
+  @node = @mindmap.new(params[:node])
     respond_to do |format| 
       if @mindmap.save
-        format.html 
+        format.html { redirect_to( 
         format.json { render => :json }
       else
         format.html { render :action => "new" }
--- app/controllers/node_controller.rb
+++ app/controllers/node_controller.rb
@@ -1,4 +1,8 @@
 class NodeController < ApplicationController
   def show
+    @mindmap = Mindmap.where(:unique_token => params[:unique_token]).first
+    respond_to do |format|
+      format.json { render :json => @mindmap 
+    end
   end
 end
