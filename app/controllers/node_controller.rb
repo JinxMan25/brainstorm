2c80a811d907ff58f1e7f4b285eb73d697df2a3a
Md Samiul Huq <samiulg3@gmail.com>
2014-07-31 11:15:42 -0700

    collect nodes

--- app/controllers/node_controller.rb
+++ app/controllers/node_controller.rb
@@ -1,6 +1,7 @@
 class NodeController < ApplicationController
   def show
     @nodes = Mindmap.find(params[:unique_token]).nodes
+    @nodes.collect{ |node| { node_id : node.id, target : node.target, source : node.source } }
     respond_to do |format|
       format.json { render :json => @nodes } 
     end
