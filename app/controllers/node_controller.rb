56e2c88dc8742e787c44bb1d52cea0897446924f
Md Samiul Huq <samiulg3@gmail.com>
2014-07-31 20:46:43 -0700

    Json obj

--- app/controllers/node_controller.rb
+++ app/controllers/node_controller.rb
@@ -1,8 +1,9 @@
 class NodeController < ApplicationController
   def show
-    @nodes = Mindmap.find(params[:unique_token]).nodes
-    @nodes.collect{ |node| { node_id : node.id, target : node.target, source : node.source } }
+    @mindmap = Mindmap.find(params[:unique_token]).nodes
+    @nodes = @mindmap.collect{ |node| { :node_id => node.id, :target => node.target, :source => node.source } }
     respond_to do |format|
+      format.html
       format.json { render :json => @nodes } 
     end
   end
