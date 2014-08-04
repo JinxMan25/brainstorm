9cb56a2c38535324e826597de48550fcdd941234
Md Samiul Huq <samiulg3@gmail.com>
2014-08-03 23:28:18 -0700

    test

--- app/controllers/node_controller.rb
+++ app/controllers/node_controller.rb
@@ -1,8 +1,11 @@
 class NodeController < ApplicationController
   def show
     @mindmap = Mindmap.find_by_unique_token(params[:unique_token])
+    @nodes = @mindmap.nodes.collect{ |node| { :node_id => node.id, :source_body => node.source_body, :target_body => node.target_body, :target => node.target, :source => node.source } }
+    byebug
     respond_to do |format|
       format.html
+      format.json { render :json => @nodes } 
     end
   end
   def new
