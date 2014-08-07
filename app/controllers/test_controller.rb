66f308ce89d4506dd783ddf1e04e5cbc839f4d64
Md Samiul Huq <samiulg3@gmail.com>
2014-08-07 15:32:52 -0400

    work in progress with updating node

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -3,5 +3,8 @@ class TestController < ApplicationController
     @mindmap = Mindmap.first
     nodes = @mindmap.nodes
     @nodes = nodes.map{|node| { target: node.target, source: node.source, source_body: node.source_body, target_body: node.target_body } }
+    @mindmap2 = Mindmap.last
+    nodes = @mindmap2.nodes
+    @nodes2 = nodes.map{|node| { target: node.target, source: node.source, source_body: node.source_body, target_body: node.target_body } }
   end
 end
