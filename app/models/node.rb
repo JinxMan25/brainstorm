96d5758fea23357aa16945c85d0f752c6b92c36a
Md Samiul Huq <samiulg3@gmail.com>
2014-09-09 22:41:56 -0400

    capistrano precompile assets to public

--- app/models/node.rb
+++ app/models/node.rb
@@ -1,7 +1,7 @@
 class Node < ActiveRecord::Base
  validates :source_body, length: { maximum: 275, too_long: "You are only allowed a maximum of 275 characters" }
  validates :target_body, length: { maximum: 275, too_long: "You are only allowed a maximum of 275 characters" }
- validates :source, :target, length: { maximum: 10, too_long: "You are only allowed a maximum of 10 characters" }
+ validates :source, :target, length: { maximum: 30, too_long: "You are only allowed a maximum of 10 characters" }
 
  belongs_to :mindmap
 
